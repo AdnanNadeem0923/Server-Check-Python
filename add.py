d8c06dd234f59198996b438526e14b5a239d9776
AdnanNadeem0923 <adnannadeem0923@gmail.com>
2023-09-22 16:15:32 +0530

    Your commit message here

---
+++ add.py
@@ -0,0 +1,18 @@
+# Simple program to add a server after you have
+
+import pickle
+from main import Server
+
+servers = pickle.load( open( "servers.pickle", "rb" ) )
+
+print("Example to add server")
+
+servername = input("enter server name")
+port = int(input("Enter a port number as integer"))
+connection = input("Enter a type ping/plain/ssl")
+priority = input("Enter priority high/low")
+
+new_server = Server(servername, port, connection, priority)
+servers.append(new_server)
+
+pickle.dump(servers, open("servers.pickle", "wb" ))
